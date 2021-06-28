98ad8f52da7f1970e4880e23b449b73b8eef5bab
yuragms <yura.gms@gmail.com>
2021-06-28 12:34:20 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Все Хорошо")
+import dishesArray from './menu.json';
+import dishesTemplates from './templates/dishcard.hbs';
+
+
+
+const dishesList = document.querySelector('.js-menu');
+const themeSwitch = document.querySelector ('#theme-switch-toggle');
+
+console.log(themeSwitch);
+
+themeSwitch.addEventListener('change', )
+
+
+
+dishesList.innerHTML = dishesTemplates(dishesArray);
+
+
+
